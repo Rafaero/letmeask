ab35e9c3dc2de519124e350a0af44fb2a0bfd364
Rafael <rafaelpaivavitor@hotmail.com>
2021-06-22 17:45:37 -0300

    Second Commit day 02 NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
+
+
   return (
-    <h1>Hello World</h1>
+    <AuthContextProvider>
+      <BrowserRouter>
+            <Route path="/"exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+      </BrowserRouter>
+    </AuthContextProvider>
+    
   );
 }
 
