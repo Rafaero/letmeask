ded2c1d5dc8f5c72cf8fa314568875d219e8a685
Rafael <rafaelpaivavitor@hotmail.com>
2021-06-23 16:00:23 -0300

    third Commit day 03  NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+
 
 import { AuthContextProvider } from './context/AuthContext';
 
@@ -9,13 +11,15 @@ function App() {
 
 
   return (
-    <AuthContextProvider>
       <BrowserRouter>
+        <AuthContextProvider>
+          <Switch>
             <Route path="/"exact component={Home} />
-            <Route path="/rooms/new" component={NewRoom} />
-      </BrowserRouter>
-    </AuthContextProvider>
-    
+            <Route path="/rooms/new" exact component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+            </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>    
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,5 @@
-// import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 // import { AuthContext } from '../context/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -7,13 +7,35 @@ import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+// import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 
 export function NewRoom(){
 
-    // const { user } = useAuth();
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(newRoom.trim() === ''){
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        history.push(`/room/${firebaseRoom.key}`)
+
+    }
 
     return(
         <div id="page-auth">
@@ -26,10 +48,12 @@ export function NewRoom(){
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input 
                             type="text"
                             placeholder="Nome da sala" 
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
